e8cadc4090c4456ba35c86c8e06037cf9d7c34dc
Syed <isyedbokhari@gmail.com>
2023-06-28 19:04:53 -0400

    added everyone's page factory, runners, stepDefinitions, feature files

--- src/test/java/Utilities/pageInitializer.java
+++ src/test/java/Utilities/pageInitializer.java
@@ -1,11 +1,35 @@
 package Utilities;
 
+import pages.AlexsFactory;
+import pages.HamadsFactory;
+import pages.NaidansFactory;
+import pages.NaransFactory;
+import pages.NurbiyesFactory;
+import pages.ShahzaibsFactory;
 import pages.SyedsFactory;
+import pages.TrooksFactory;
+import pages.ZakirsFactory;
 
 public class pageInitializer extends driverClass {
 	
 	public static SyedsFactory syedsfactory;
+	public static HamadsFactory hamadsfactory;
+	public static ShahzaibsFactory shahzaibsfactory;
+	public static ZakirsFactory zakirsfactory;
+	public static NurbiyesFactory nurbiyesfactory;
+	public static TrooksFactory trooksfactory;
+	public static NaidansFactory naidansfactory;
+	public static NaransFactory naransfactory;
+	public static AlexsFactory alexsfactory;
 	public static void initialize() {
 	   syedsfactory = new SyedsFactory();
+	   hamadsfactory = new HamadsFactory();
+	   shahzaibsfactory = new ShahzaibsFactory();
+	   zakirsfactory = new ZakirsFactory();
+	   nurbiyesfactory = new NurbiyesFactory();
+	   trooksfactory = new TrooksFactory();
+	   naidansfactory = new NaidansFactory();
+	   naransfactory = new NaransFactory();
+	   alexsfactory = new AlexsFactory();
 	}
 }
---
+++ src/test/java/runners/ZakirsRunner.java
@@ -0,0 +1,18 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith (Cucumber.class)
+@CucumberOptions(
+		plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json"},
+		features = "src/test/resources/features/ZakirsFeatures.feature",
+		glue = "stepDefinition",
+		dryRun = false,
+		tags = ""
+		)
+public class ZakirsRunner {
+
+}
