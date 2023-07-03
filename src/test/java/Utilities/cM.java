6a8a2e6573b31804b3b79b25cdb4db3ae78d6fd9
Naidan Jigjiddorj <Naidan.jigjiddorj@gmail.com>
2023-07-03 16:43:49 -0500

    test cases by Naidan

--- src/test/java/Utilities/cM.java
+++ src/test/java/Utilities/cM.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Set;
 
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchFrameException;
 import org.openqa.selenium.WebElement;
@@ -13,7 +15,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.UnexpectedTagNameException;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class  cM  {
+public class  cM{
 
 	// This method is for sending String to the element we have given
 	// @param : WebElement element = is accepting the WebElement where we
@@ -247,6 +249,11 @@ public class  cM  {
 		
 		waitForVisibilty(element).click();
 	}
+	//Created by Naidan
+	public void clickElement(WebElement element) {
+		element.click();
+	}
+	
 	
 	public static void sendKey ( String keys, WebElement element) {
 		
@@ -270,8 +277,30 @@ public class  cM  {
 		actions.doubleClick().perform();
 	}
 	
+//JS executor click By Naidan
 	
-	
+	public static void jsClick(WebElement element) {
+		try {
+		    // Attempt to click on the element using the regular Selenium click() method
+			element.click();
+		} catch (ElementClickInterceptedException e) {
+		    // If ElementClickInterceptedException is caught, handle it using JavaScript Executor
+
+		    // Create an instance of JavascriptExecutor
+			JavascriptExecutor js = (JavascriptExecutor) driverClass.getDriver();
+
+		    try {
+		        // Scroll the page to bring the element into view
+		    	js.executeScript("arguments[0].scrollIntoView(true);", element);
+
+		        // Click on the element using JavaScript
+		    	js.executeScript("arguments[0].click();", element);
+		    } catch (Exception ex) {
+		        // Handle any exceptions that may occur during the JavaScript execution
+		        ex.printStackTrace();
+		    }
+		}
+	}
 	
 	
 	
---
+++ src/test/resources/features/featuresByNaidan/shopNowAdNaidan.feature
@@ -0,0 +1,8 @@
+Feature: Shop Now ad is clickable
+
+
+	  Scenario: Top Deals drop down sorting
+    Given Run Petco Login Steps
+    When click on Deal tab
+    Then  click on Shop Now
+    And Verify that user is able to click on Shop Now
