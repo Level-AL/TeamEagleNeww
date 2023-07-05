ec17b1101279bb70d2cb71462ea89b9aa0feed92
shahzaib930 <shahzaibhussain275@gmail.com>
2023-07-05 18:50:52 -0400

    Shaizaib final commit all testcases

--- src/test/java/runners/ShahzaibsRunner.java
+++ src/test/java/runners/ShahzaibsRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 		plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json"},
 		features = "src/test/resources/features/ShahzaibsFeatures.feature",
 		glue = "stepDefinition",
-		dryRun = false,
+		dryRun = true,
 		tags = ""
 		)
 public class ShahzaibsRunner {
