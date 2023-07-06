7d9369811afb1098178b6589096e52c6a862a8b2
trookiep <trookiep@gmail.com>
2023-07-06 13:51:33 -0400

    no more hard code

--- src/test/java/stepDefinition/TrooksStepDefinitions.java
+++ src/test/java/stepDefinition/TrooksStepDefinitions.java
@@ -3,12 +3,10 @@ package stepDefinition;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 
+import Utilities.ConfigurationReader;
 import Utilities.cM;
-import Utilities.driverClass;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -17,42 +15,36 @@ public class TrooksStepDefinitions extends cM {
 
 	@Given("I am on the Petco website")
 	public void navigateToPetcoWebsite() {
-		driverClass.getDriver().get("https://www.petco.com");
+		getDriver();
 	}
 
 	@When("I search for {string}")
 	public void searchFor(String searchItem) {
-		WebElement searchInput = driverClass.getDriver().findElement(By.id("header-search"));
-		searchInput.sendKeys(searchItem);
-		WebElement searchButton = driverClass.getDriver().findElement(By.xpath("//button[@aria-label='Search']"));
-		searchButton.click();
-
+		tf.searchBar.sendKeys(searchItem);
+		tf.searchButton.click();
 	}
 
 	@Then("I should see search results related to {string}")
 	public void verifySearchResults(String searchResult) {
-		String pageTitle = driverClass.getDriver().getTitle();
-		Assert.assertTrue("Search results page is not displayed", pageTitle.contains(searchResult));
+		Assert.assertTrue(ConfigurationReader.getProperty("DisplayErrorMessage"),
+				getDriver().getTitle().contains(searchResult));
 	}
 
 	@Then("I click on same day delivery tab under Category")
 	public void i_click_on_same_day_delivery_tab_under_category() {
-		WebElement samedaybutton = driverClass.getDriver().findElement(By.xpath(
-				"//*[@href='/shop/en/petcostore/search?query=small%20pet%20food&filters%5Bgroup_id%5D=same-day-delivery-products']"));
-		samedaybutton.click();
-
+		tf.sameDayButtonUnderCategoryTab.click();
 	}
 
 	@Then("I should see search results related to {string} under same day delivery category")
 	public void i_should_see_search_results_related_to_under_same_day_delivery_category(String string) {
-		String pageTitle = driverClass.getDriver().getTitle();
-		Assert.assertTrue("Search results page is not displayed", pageTitle.contains(string));
+		Assert.assertTrue(ConfigurationReader.getProperty("DisplayErrorMessage"),
+				getDriver().getTitle().contains(string));
 	}
 
 	@Then("I should scroll down")
 	public void i_should_scroll_down() {
 		// Scroll down to the bottom of the page
-		JavascriptExecutor js = (JavascriptExecutor) driverClass.getDriver();
+		JavascriptExecutor js = (JavascriptExecutor) getDriver();
 		js.executeScript("window.scrollBy(0,document.body.scrollHeight)", "");
 
 		// Wait for a brief moment after scrolling
@@ -65,31 +57,24 @@ public class TrooksStepDefinitions extends cM {
 
 	@Then("I should see the other items while scrolling down related to {string}")
 	public void i_should_see_the_other_items_while_scrolling_down_related_to(String string) {
-		String pageTitle = driverClass.getDriver().getTitle();
-		Assert.assertTrue("Search results page is not displayed", pageTitle.contains(string));
+		Assert.assertTrue(ConfigurationReader.getProperty("DisplayErrorMessage"),
+				getDriver().getTitle().contains(string));
 	}
 
 	@When("I open triple bar icon")
 	public void i_open_triple_bar_icon() {
-		WebElement triplebarbutton = driverClass.getDriver()
-				.findElement(By.xpath("//button[@data-testid='hamburger-menu']"));
-		triplebarbutton.click();
+		tf.triplebarbutton.click();
 	}
 
 	@Then("I click on small pet tab")
 	public void i_click_on_small_pet_tab() {
-		WebElement shopsmallpetbutton = driverClass.getDriver()
-				.findElement(By.xpath("//button[@aria-label='Shop SMALL PET']"));
-		shopsmallpetbutton.click();
+		tf.shopSmallPetButtonInsideTripleBar.click();
 	}
 
 	@Then("I should see the sub page related to small pet")
 	public void i_should_see_the_sub_page_related_to_small_pet() {
-		Assert.assertTrue("Search results page is not displayed", driverClass.getDriver().findElement(By.xpath(
-				"//li[@role='listitem'][7]//section[@class='flyout-styled__FlyoutContent-sc-317a9e89-1 itkdkh']/h2"))
-				.isDisplayed());
+		Assert.assertTrue(ConfigurationReader.getProperty("DisplayErrorMessage"),
+				tf.smallPetSubPageTitle.isDisplayed());
 	}
 
 }
-
-
--- src/test/resources/features/TrooksFeatures.feature
+++ src/test/resources/features/TrooksFeatures.feature
@@ -1,21 +1,23 @@
-Feature: petco Search
+Feature: Petco SearchBar Functionality
+  Scenario Background:
+  Given I am on the Petco website
 
- Scenario: Search for small pet
-    Given I am on the Petco website
+  @Test1
+  Scenario: Search for small pet
     When I search for "small pet"
     Then I should see search results related to "small pet"
-    
+
+  @Test2
   Scenario: Small pet same day delivery items displayed
-    Given I am on the Petco website
     When I search for "small pet food"
     Then I should see search results related to "small pet food"
     And I click on same day delivery tab under Category
     Then I should see search results related to "small pet food" under same day delivery category
     Then I should scroll down
     Then I should see the other items while scrolling down related to "small pet food"
-    
-  Scenario: Look for small pet in Categories triple bar 
-    Given I am on the Petco website
-    When  I open triple bar icon
+
+  @Test3
+  Scenario: Look for small pet in Categories triple bar
+    When I open triple bar icon
     Then I click on small pet tab
     Then I should see the sub page related to small pet
